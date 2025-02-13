3abc290b62db34c836a07ec227cd714fa4fae9c0
Saugat Chand <saugatchand96@gmail.com>
2025-02-13 16:38:43 +0545

    class

---
+++ js/loop.js
@@ -0,0 +1,19 @@
+// // loop
+// // executing same logic over and over
+
+// for
+// do...while
+// while..
+
+//syntax
+// for (start;check;increment/decrement){}
+//Multiplication
+
+// for (let i = 1; i <= 100; i++) {
+//   console.log(``);
+// }
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+  sum = sum + i;
+}
+console.log(sum);
