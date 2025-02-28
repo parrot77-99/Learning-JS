c177a61409865305f390d10380a429dff3cfde12
Saugat Chand <saugatchand96@gmail.com>
2025-03-01 01:12:24 +0545

    learned interactions

--- practice/newpractice.js
+++ practice/newpractice.js
@@ -1,10 +1,50 @@
 console.log("hello");
 
-const person = "Saugat";
+// const person = "Saugat";
+// console.log(person);
+
+// let person1 = "unique";
+// console.log(person1);
+
+// var person2 = "raktim";
+// console.log(person2);
+
+const name = "saugat";
+console.log(name);
+
+var person = "ram"; //variable declare
+var person = "hari"; //override
 console.log(person);
 
-let person1 = "unique";
-console.log(person1);
+let school = "rkp";
+school = "ghi";
+console.log(school);
 
-var person2 = "raktim";
-console.log(person2);
+// alert("hello world");
+
+// prompt("what is your name?");
+
+// const teacher = prompt("what is the name of coding teacher?");
+// console.log(teacher);
+
+// const ceo = prompt("who is the ceo of tesla company");
+// console.log(ceo);
+
+// const isceo = confirm("Tesla company ceo is elon musk?");
+// console.log(isceo);
+
+// const PM = confirm("KP oli is the PM of Nepal?");
+// console.log(PM);
+
+// const user = prompt("What is your name");
+// console.log(user);
+// alert("welcome saugat we are very happy that you came back");
+
+//1
+// const user = prompt("what is your name");
+// const greeter = "Welcome back," + user;
+// alert(greeter);
+
+//String literal
+const username = prompt("what is your name");
+alert(`Welcome back, ${username}`);
